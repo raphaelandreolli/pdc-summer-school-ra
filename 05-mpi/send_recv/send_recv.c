c2060e3bf173beb24d2846ca85e92d8f97ec3163
Raphael Andreolli <raphael.andreolli@hotmail.com>
2023-08-21 14:10:30 +0200

    Update send_recv.c

--- 05-mpi/send_recv/send_recv.c
+++ 05-mpi/send_recv/send_recv.c
@@ -7,19 +7,27 @@ int main(int argc, char *argv[] )
   MPI_Status status;
 
   //Add call to mpi init
+  MPI_Init(&argc,&argv);
   //Add call to get rank
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
   //Add call to get size of communicator
+  MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
 
   //Replace the commented lines with MPI calls
   if (rank == 0) {
     value = 5;
     printf( "Process %d sending %d\n", rank, value );
     //Insert MPI command to send value to the next rank
+    // MPI_Send (buf, count, datatype, dest, tag, comm)
+    MPI_Send(value, 100, MPI_INT, rank+1, 20, MPI_COMM_WORLD);
   } else {
     //Insert MPI command to receive value from the previous rank
+    // MPI_Recv (buf, count, datatype, source, tag, comm, status)
+    MPI_Recv (value, 100, MPI_INT, rank-1, 20, MPI_COMM_WORLD, &status);
     printf( "Process %d got %d\n", rank, value );
     if (rank < size - 1)
       //Insert MPI command to send value to the next rank
+      MPI_Send(value, 100, MPI_INT, rank+1, 20, MPI_COMM_WORLD );
   }
 
   MPI_Finalize( );
